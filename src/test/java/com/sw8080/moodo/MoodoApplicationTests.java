67fb8c9f7ed63b75c81b06803ac1a54ee5da757c
thdgus0307 <bsh0374@gmail.com>
2026-05-13 15:22:52 +0900

    remote

---
+++ src/test/java/com/sw8080/moodo/MoodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sw8080.moodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
